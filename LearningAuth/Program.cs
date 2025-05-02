4f8f592bca8082ed17536f27e3584eb49857784e
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-05-02 10:46:40 +0200

    New endpoint + roles

--- LearningAuth/Program.cs
+++ LearningAuth/Program.cs
@@ -1,4 +1,7 @@
 // dotnet user-jwts create
+
+using System.Security.Claims;
+
 Dictionary<string, List<string>> gamesMap = new()
 {
     { "player1", new List<string>() { "Tekken 8", "GTA V" } },
@@ -12,7 +15,26 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+// dotnet user-jwts create --role admin
 app.MapGet("/playergames", () => gamesMap)
-    .RequireAuthorization();
+    .RequireAuthorization(policy =>
+    {
+        policy.RequireRole("admin");
+    });
 
+// dotnet user-jwts create --role player -n player1
+app.MapGet("/mygames", (ClaimsPrincipal user) =>
+{
+    ArgumentNullException.ThrowIfNull(user.Identity?.Name);
+    var username = user.Identity.Name;
+    if (!gamesMap.ContainsKey(username))
+    {
+        return Results.Empty;
+    }
+    return Results.Ok(gamesMap[username]);
+})
+.RequireAuthorization(policy =>
+{
+    policy.RequireRole("player");
+});
 app.Run();

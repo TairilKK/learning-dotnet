028ed57d6a06f6db61cb084cd006a93e12c241de
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-05-12 12:10:29 +0200

    Annotations => FluentAPI

--- LearningEntityFramework/Entities/MyBoardsContext.cs
+++ LearningEntityFramework/Entities/MyBoardsContext.cs
@@ -9,4 +9,21 @@ public class MyBoardsContext(DbContextOptions<MyBoardsContext> options): DbConte
     public DbSet<Tag> Tags { get; set; }
     public DbSet<Comment> Comments { get; set; }
     public DbSet<Address> Addresses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<WorkItem>()
+            .Property(x => x.State)
+            .IsRequired();
+
+        modelBuilder.Entity<WorkItem>(eb =>
+        {
+            eb.Property(wi => wi.Area).HasColumnType("varchar(200)");
+            eb.Property(wi => wi.IterationPath).HasColumnName("Iteration_Path");
+            eb.Property(wi => wi.EndDate).HasPrecision(3);
+            eb.Property(wi => wi.Efford).HasColumnType("decimal(5,2)");
+            eb.Property(wi => wi.Activity).HasMaxLength(200);
+            eb.Property(wi => wi.RemainingWork).HasPrecision(14, 2);
+        });
+    }
 }
--- LearningEntityFramework/Entities/WorkItem.cs
+++ LearningEntityFramework/Entities/WorkItem.cs
@@ -7,24 +7,17 @@ namespace LearningEntityFramework.Entities;
 public class WorkItem
 {
     public int Id { get; set; }
-    [Required]
     public string State { get; set; }
-    [Column(TypeName = "varchar(200)")]
     public string Area { get; set; }
-    [Column("Iteraion_Path")]
     public string IterationPath { get; set; }
     public int Priority { get; set; }
     // Epic
     public DateTime? StartDate { get; set; }
-    [Precision(3)]
     public DateTime? EndDate { get; set; }
     // Issue
-    [Column(TypeName = "decimal(5,2)")]
     public decimal Efford { get; set; }
     // Task
-    [MaxLength(200)]
     public string Activity { get; set; }
-    [Precision(14, 2)]
     public decimal RemainingWork { get; set; }
     public string Type { get; set; }
 }

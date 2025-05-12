f40a35127c26b46dabfd8cb552940e5a6dbf8518
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-05-12 14:36:30 +0200

    Creating new migration

--- LearningEntityFramework/Entities/Tag.cs
+++ LearningEntityFramework/Entities/Tag.cs
@@ -4,6 +4,6 @@ public class Tag
 {
     public int Id { get; set; }
     public string Value { get; set; }
-
+    public string Category { get; set; }
     public List<WorkItem> WorkItems { get; set; } = new List<WorkItem>();
 }

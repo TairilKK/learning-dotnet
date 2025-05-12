40f34a10d0658cffeb9b7d98c7ef0db003452819
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-05-12 11:48:15 +0200

    Creating Context

---
+++ LearningEntityFramework/Entities/MyBoardsContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LearningEntityFramework.Entities;
+
+public class MyBoardsContext(DbContextOptions<MyBoardsContext> options): DbContext(options)
+{
+    public DbSet<WorkItem> WorkItems { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<Address> Addresses { get; set; }
+}
--- LearningEntityFramework/Program.cs
+++ LearningEntityFramework/Program.cs
@@ -1,8 +1,13 @@
+using LearningEntityFramework.Entities;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<MyBoardsContext>(
+    option => option.UseNpgsql(builder.Configuration.GetConnectionString("MyBoardsDbConnectionString"))
+);
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())

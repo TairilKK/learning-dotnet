e23be71721cc6028a678a3c2f20c2019046200d9
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-05-14 16:55:42 +0200

    Entity Framework: Database First

---
+++ LearningEntityFrameworkNorthwind/Data/Migrations/ProductsAboveAveragePrice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningEntityFrameworkNorthwind.Data.Migrations;
+
+public partial class ProductsAboveAveragePrice
+{
+    public string ProductName { get; set; } = null!;
+
+    public decimal? UnitPrice { get; set; }
+}

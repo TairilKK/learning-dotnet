a0a06b8219e5c7450be43f39ee516023edf16cf6
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-05-15 12:47:31 +0200

    GetResult test

---
+++ LearningTest.Tests/BmiCalculatorFacadeTests.cs
@@ -0,0 +1,23 @@
+﻿using LearningTest.Model;
+
+namespace LearningTest.Tests;
+
+public class BmiCalculatorFacadeTests
+{
+    private const string OVERWEIGHT_SUMMARY = "You are a bit overweight";
+
+    [Fact]
+    public void GetResult_ForValidInputs_ReturnCorrectResult()
+    {
+        var bmiCalculatorFacade = new BmiCalculatorFacade(UnitSystem.Metric);
+
+        double weight = 90;
+        double height = 190;
+
+        var result = bmiCalculatorFacade.GetResult(weight, height);
+
+        Assert.Equal(24.93, result.Bmi);
+        Assert.Equal(BmiClassification.Overweight, result.BmiClassification);
+        Assert.Equal(OVERWEIGHT_SUMMARY, result.Summary);
+    }
+}

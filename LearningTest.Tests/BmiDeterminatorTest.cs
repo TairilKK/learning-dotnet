280a6c173921e45f8a64b7576acd996a2a81e960
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-05-15 11:44:10 +0200

    1st test to learningTest project

---
+++ LearningTest.Tests/BmiDeterminatorTest.cs
@@ -0,0 +1,17 @@
+﻿using LearningTest.Model;
+
+namespace LearningTest.Tests;
+
+public class BmiDeterminatorTest
+{
+    [Fact]
+    public void DetermineBmi_ForBmiBelow18_5_ReturnsUnderweightClassification()
+    {
+        var bmiDeterminator = new BmiDeterminator();
+        double bmi = 10;
+
+        var result = bmiDeterminator.DetermineBmi(bmi);
+
+        Assert.Equal(BmiClassification.Underweight, result);
+    }
+}

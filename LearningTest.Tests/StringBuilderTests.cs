87964aeef04ce2e860c973e2d835ee29591c91b5
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-05-15 11:35:21 +0200

    New test project + 1st test

---
+++ LearningTest.Tests/StringBuilderTests.cs
@@ -0,0 +1,22 @@
+using System.Text;
+
+namespace LearningTest.Tests;
+
+public class StringBuilderTests
+{
+    [Fact]
+    public void Append_ForTwoStrings_ReturnsConcatenatedString()
+    {
+        // arrange
+        var sb = new StringBuilder();
+
+        // act
+        sb.Append("test")
+            .Append("test2");
+
+        var result = sb.ToString();
+
+        // assert
+        Assert.Equal("testtest2", result);
+    }
+}

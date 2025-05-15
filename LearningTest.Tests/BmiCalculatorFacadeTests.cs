3bcbb203147a9e5c34a01971fc36880e7b11df9a
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-05-15 12:50:37 +0200

    Using fluent assertion

--- LearningTest.Tests/BmiCalculatorFacadeTests.cs
+++ LearningTest.Tests/BmiCalculatorFacadeTests.cs
@@ -1,4 +1,5 @@
-﻿using LearningTest.Model;
+﻿using FluentAssertions;
+using LearningTest.Model;
 
 namespace LearningTest.Tests;
 
@@ -16,8 +17,12 @@ public class BmiCalculatorFacadeTests
 
         var result = bmiCalculatorFacade.GetResult(weight, height);
 
-        Assert.Equal(24.93, result.Bmi);
-        Assert.Equal(BmiClassification.Overweight, result.BmiClassification);
-        Assert.Equal(OVERWEIGHT_SUMMARY, result.Summary);
+        // Assert.Equal(24.93, result.Bmi);
+        // Assert.Equal(BmiClassification.Overweight, result.BmiClassification);
+        // Assert.Equal(OVERWEIGHT_SUMMARY, result.Summary);
+
+        result.Bmi.Should().Be(24.93);
+        result.BmiClassification.Should().Be(BmiClassification.Overweight);
+        result.Summary.Should().Be(OVERWEIGHT_SUMMARY);
     }
 }

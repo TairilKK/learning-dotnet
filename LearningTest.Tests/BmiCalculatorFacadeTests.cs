cd06e3a982171e676eb3cc014b21fa83b9c48324
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-05-18 14:10:54 +0200

    ITestOutputHelper

--- LearningTest.Tests/BmiCalculatorFacadeTests.cs
+++ LearningTest.Tests/BmiCalculatorFacadeTests.cs
@@ -1,11 +1,19 @@
 ﻿using FluentAssertions;
 using LearningTest.Model;
 using Moq;
+using Xunit.Abstractions;
 
 namespace LearningTest.Tests;
 
 public class BmiCalculatorFacadeTests
 {
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public BmiCalculatorFacadeTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
     private const string UnderweightSummary = "You are underweight, you should put on some weight";
     private const string NormalSummary = "Your weight is normal, keep it up";
     private const string OverweightSummary = "You are a bit overweight";
@@ -28,6 +36,7 @@ public class BmiCalculatorFacadeTests
 
         var result = bmiCalculatorFacade.GetResult(1, 1);
 
+        _testOutputHelper.WriteLine($"For classification: {bmiClassification} the result is: {result.Summary}");
         result.Summary.Should().Be(expectedResult);
     }
 }

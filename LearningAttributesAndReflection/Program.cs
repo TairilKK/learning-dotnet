f319ba66029a14b2c887925588ae88163038b04e
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-06-03 10:33:13 +0200

    Display obj method

---
+++ LearningAttributesAndReflection/Program.cs
@@ -0,0 +1,37 @@
+﻿using LearningAttributesAndReflection;
+using System.Reflection;
+
+void Display(object obj, string prefix="")
+{
+    Type objType = obj.GetType();
+    PropertyInfo[] props = objType.GetProperties();
+
+    Console.WriteLine($"{prefix}{objType.Name}:");
+    foreach(var prop in props)
+    {
+        var value = prop.GetValue(obj);
+        Type valType = value.GetType();
+        
+        if(valType.IsPrimitive || valType == typeof(string))
+            Console.WriteLine($"{prefix}  {prop.Name}: {value}");
+        else
+            Display(value, $"{prefix}  ");
+    }
+    Console.WriteLine("");
+}
+
+var add1 = new Address()
+{
+    City = "Warsaw",
+    Street = "Długa"
+};
+
+var user1 = new User()
+{
+    Address = add1,
+    FirstName = "Jack",
+    LastName = "Sparrow"
+};
+
+Display(add1);
+Display(user1);

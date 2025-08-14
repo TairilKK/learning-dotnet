5b909005107e8aabab8614c5f793cf3609d71183
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-08-14 13:35:47 +0200

    Add connection from client

---
+++ LearningSignalR/wwwroot/js/usersCount.js
@@ -0,0 +1,26 @@
+﻿// create connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+// connect to methods that hub invokes
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.querySelector("#totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+})
+
+// invoke hub methods
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+// start connection
+function fulfilled() {
+    // do something on start
+    console.log("Connection to User Hub successful!");
+}
+
+function rejected() {
+    // rejected logs
+    console.log("Connection to User Hub unsuccessful :(");
+}
+
+connectionUserCount.start().then(fulfilled, rejected);

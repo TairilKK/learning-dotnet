07c7eaaf87b9e152285ce0910247e5b358823ac2
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-08-15 10:31:09 +0200

    Add signalR groups

---
+++ LearningSignalR/Hubs/HouseGroupHub.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace LearningSignalR.Hubs
+{
+    public class HouseGroupHub : Hub
+    {
+        public static List<string> GroupsJoined { get; set; } = new List<string>();
+        public async Task JoinGroup(string groupName)
+        {
+            if (!GroupsJoined.Contains(Context.ConnectionId + ":" + groupName))
+            {
+                GroupsJoined.Add(Context.ConnectionId + ":" + groupName);
+
+                string groupList = "";
+                foreach (var str in GroupsJoined)
+                {
+                    if (str.Contains(Context.ConnectionId))
+                    {
+                        groupList += str.Split(':')[1] + " ";
+                    }
+                }
+
+                await Clients.Caller.SendAsync("subscitpionStatus", groupList, groupName.ToLower(), true );
+                await Clients.Others.SendAsync("memberAddedToHouse", groupName);
+
+                await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            }
+        }
+        public async Task LeaveGroup(string groupName)
+        {
+            if (GroupsJoined.Contains(Context.ConnectionId + ":" + groupName))
+            {
+                GroupsJoined.Remove(Context.ConnectionId + ":" + groupName);
+
+                string groupList = "";
+                foreach (var str in GroupsJoined)
+                {
+                    if (str.Contains(Context.ConnectionId))
+                    {
+                        groupList += str.Split(':')[1] + " ";
+                    }
+                }
+
+                await Clients.Caller.SendAsync("subscitpionStatus", groupList, groupName.ToLower(), false );
+                await Clients.Others.SendAsync("memberRemovedFromHouse", groupName);
+
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+            }
+        }
+
+        public async Task TriggerGroupNotify(string groupName)
+        {
+            await Clients.Group(groupName).SendAsync("tiggerHouseNotification", groupName);
+        }
+    }
+}
---
+++ LearningSignalR/wwwroot/js/houseGroup.js
@@ -0,0 +1,157 @@
+﻿let lbl_houseJoined = document.getElementById("lbl_houseJoined");
+
+let btn_un_gryffindor = document.getElementById("btn_un_gryffindor");
+let btn_un_slytherin = document.getElementById("btn_un_slytherin");
+let btn_un_hufflepuff = document.getElementById("btn_un_hufflepuff");
+let btn_un_ravenclaw = document.getElementById("btn_un_ravenclaw");
+
+let btn_gryffindor = document.getElementById("btn_gryffindor");
+let btn_slytherin = document.getElementById("btn_slytherin");
+let btn_hufflepuff = document.getElementById("btn_hufflepuff");
+let btn_ravenclaw = document.getElementById("btn_ravenclaw");
+
+let trigger_gryffindor = document.getElementById("trigger_gryffindor");
+let trigger_slytherin = document.getElementById("trigger_slytherin");
+let trigger_hufflepuff = document.getElementById("trigger_hufflepuff");
+let trigger_ravenclaw = document.getElementById("trigger_ravenclaw");
+
+var connectionHouseGroup = new signalR.HubConnectionBuilder()
+    .withUrl("/hubs/houseGroup", signalR.HttpTransportType.WebSockets).build();
+
+btn_gryffindor.addEventListener("click", (event) => {
+    event.preventDefault();
+    connectionHouseGroup.send("JoinGroup", "Gryffindor");
+})
+
+btn_hufflepuff.addEventListener("click", (event) => {
+    event.preventDefault();
+    connectionHouseGroup.send("JoinGroup", "Hufflepuff");
+})
+
+btn_ravenclaw.addEventListener("click", (event) => {
+    event.preventDefault();
+    connectionHouseGroup.send("JoinGroup", "Ravenclaw");
+})
+
+btn_slytherin.addEventListener("click", (event) => {
+    event.preventDefault();
+    connectionHouseGroup.send("JoinGroup", "Slytherin");
+})
+
+btn_un_gryffindor.addEventListener("click", (event) => {
+    event.preventDefault();
+    connectionHouseGroup.send("LeaveGroup", "Gryffindor");
+})
+
+btn_un_hufflepuff.addEventListener("click", (event) => {
+    event.preventDefault();
+    connectionHouseGroup.send("LeaveGroup", "Hufflepuff");
+})
+
+btn_un_ravenclaw.addEventListener("click", (event) => {
+    event.preventDefault();
+    connectionHouseGroup.send("LeaveGroup", "Ravenclaw");
+})
+
+btn_un_slytherin.addEventListener("click", (event) => {
+    event.preventDefault();
+    connectionHouseGroup.send("LeaveGroup", "Slytherin");
+})
+
+trigger_gryffindor.addEventListener("click", (event) => {
+    event.preventDefault();
+    connectionHouseGroup.send("TriggerGroupNotify", "Gryffindor");
+})
+
+trigger_hufflepuff.addEventListener("click", (event) => {
+    event.preventDefault();
+    connectionHouseGroup.send("TriggerGroupNotify", "Hufflepuff");
+})
+
+trigger_ravenclaw.addEventListener("click", (event) => {
+    event.preventDefault();
+    connectionHouseGroup.send("TriggerGroupNotify", "Ravenclaw");
+})
+
+trigger_slytherin.addEventListener("click", (event) => {
+    event.preventDefault();
+    connectionHouseGroup.send("TriggerGroupNotify", "Slytherin");
+})
+
+connectionHouseGroup.on("tiggerHouseNotification", houseName => {
+    toastr.success(`A new notification for ${houseName} has been launched.`);
+})
+
+connectionHouseGroup.on("memberAddedToHouse", houseName => {
+    toastr.success("New member has joined the " + houseName + " house!");
+})
+
+connectionHouseGroup.on("memberRemovedFromHouse", houseName => {
+    toastr.success("Member has left the " + houseName + " house!");
+})
+
+connectionHouseGroup.on("subscitpionStatus", (strGroupsJoined, houseName, hasSubscribed) => {
+    lbl_houseJoined.innerText = strGroupsJoined;
+    if (hasSubscribed) {
+        switch (houseName) {
+            case "slytherin":
+                btn_slytherin.style.display = "none";
+                btn_un_slytherin.style.display = "";
+                break;
+            case "gryffindor":
+                btn_gryffindor.style.display = "none";
+                btn_un_gryffindor.style.display = "";
+                break;
+            case "hufflepuff":
+                btn_hufflepuff.style.display = "none";
+                btn_un_hufflepuff.style.display = "";
+                break;
+            case "ravenclaw":
+                btn_ravenclaw.style.display = "none";
+                btn_un_ravenclaw.style.display = "";
+                break;
+            default:
+                break;
+        }
+        toastr.success("You have joined the " + houseName + " house!");
+    } else {
+        switch (houseName) {
+            case "slytherin":
+                btn_slytherin.style.display = "";
+                btn_un_slytherin.style.display = "none";
+                break;
+            case "gryffindor":
+                btn_gryffindor.style.display = "";
+                btn_un_gryffindor.style.display = "none";
+                break;
+            case "hufflepuff":
+                btn_hufflepuff.style.display = "";
+                btn_un_hufflepuff.style.display = "none";
+                break;
+            case "ravenclaw":
+                btn_ravenclaw.style.display = "";
+                btn_un_ravenclaw.style.display = "none";
+                break;
+            default:
+                break;
+        }
+        toastr.success("You have left the " + houseName + " house!");
+
+    }
+})
+
+
+function fulfilled() {
+    connectionHouseGroup.invoke("GetRaceStatus").then((raceCounter) => {
+        cloakSpan.innerText = raceCounter.cloak.toString();
+        stoneSpan.innerText = raceCounter.stone.toString();
+        wandSpan.innerText = raceCounter.wand.toString();
+    })
+    console.log("Connection to User Hub successful!");
+}
+function rejected() {
+    console.log("Connection to User Hub unsuccessful :(");
+}
+
+connectionHouseGroup.start().then(fulfilled, rejected);
+
